deab3d95af4a710d1ecd89af11e3e191f80998a4
Harshit Sharma <hs414171@gmail.com>
2023-12-13 15:35:11 +0530

    Connecting MongoDb to the server

--- main.go
+++ main.go
@@ -1,18 +1,45 @@
 package main
 
-import "gofr.dev/pkg/gofr"
+import (
+    "context"
+    "fmt"
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+    "gofr.dev/pkg/gofr"
+    "github.com/joho/godotenv"
+    "log"
+    "os"
+)
 
 func main() {
-    // initialise gofr object
+    
+    if err := godotenv.Load("configs/.env"); err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    mongoURI := os.Getenv("MONGO_URI")
+
+    opts := options.Client().ApplyURI(mongoURI)
+    client, err := mongo.Connect(context.TODO(), opts)
+    if err != nil {
+        panic(err)
+    }
+    defer func() {
+        if err := client.Disconnect(context.TODO()); err != nil {
+            panic(err)
+        }
+    }()
+    
+    if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+        panic(err)
+    }
+    fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
     app := gofr.New()
-
-    // register route greet
     app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
-
         return "Hello World!", nil
     })
 
-    // Starts the server, it will listen on the default port 8000.
-    // it can be over-ridden through configs
+    
     app.Start()
 }
